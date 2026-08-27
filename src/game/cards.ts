0c0191383810991599db9443e4d5513cfc1f3ddc
Maciej Lenartowicz <lenarmathias@gmail.com>
2026-08-27 18:58:23 +0200

    feat: add card data

---
+++ src/game/cards.ts
@@ -0,0 +1,17 @@
+export type Card = {
+  id: number;
+  name: string;
+  icon: string;
+};
+
+export const deck: Card[] = [
+  { id: 1, name: "Sun", icon: "1" },
+  { id: 2, name: "Moon", icon: "2" },
+  { id: 3, name: "Star", icon: "3" },
+  { id: 4, name: "Heart", icon: "4" },
+  { id: 5, name: "Tree", icon: "5" },
+  { id: 6, name: "Flower", icon: "6" },
+  { id: 7, name: "Butterfly", icon: "7" },
+  { id: 8, name: "Cat", icon: "8" },
+  { id: 9, name: "Dog", icon: "9" }
+];
